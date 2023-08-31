bb97dab0912c70fdd5bf13bbec1984db8e8aa26e
AdnanSakib04 <navidadnan04@gmail.com>
2023-08-31 22:43:37 +0600

    tabs created

--- phtube.js
+++ phtube.js
@@ -1,1 +1,24 @@
-console.log('hey');
+const handleCategory = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    console.log(data.data);
+
+    const tabsContainer = document.getElementById('tabs-container');
+
+    data.data.forEach((category) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a onclick="handleLoadCategory('${category.category_id}')" class="btn w-[104px] tab bg-[#25252526]">${category.category}</a>
+        `;
+        tabsContainer.appendChild(div);
+    })
+
+}
+
+
+
+const handleLoadCategory = async (id) => {
+    console.log(id);
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+}
+handleCategory();
