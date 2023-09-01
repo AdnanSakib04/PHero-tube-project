12d28f8d66369acacb207a0aee3f8df22581e21d
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-01 11:30:59 +0600

    cards container partially done and no content done

--- phtube.js
+++ phtube.js
@@ -18,7 +18,75 @@ const handleCategory = async () => {
 
 
 const handleLoadCategory = async (id) => {
-    console.log(id);
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await response.json();
+
+    const cardContainer = document.getElementById('cards-container');
+    cardContainer.innerHTML = "";
+
+    const noContentContainer = document.getElementById('no-content-container');
+    noContentContainer.innerHTML = "";
+
+    console.log('--------------length');
+    console.log(data.data.length);
+
+    const checkLength = data.data.length;
+    if (checkLength > 0) {
+        data.data.forEach((x) => {
+            console.log(x.thumbnail);
+            console.log(x.title);
+            console.log(x.authors[0].profile_picture);
+            console.log(x.authors[0].profile_name);
+            console.log(x.authors[0].verified);
+            console.log(x.others.views);
+            console.log(x.others.posted_date);
+
+            const cardDiv = document.createElement('div');
+            cardDiv.innerHTML = `
+            <!-- card 1 -->
+            <div class="card card-compact w-[312px] h-[300px] bg-base-100 shadow-xl relative">
+                <figure><img  src="${x.thumbnail}" alt="" /></figure>
+                <div class="p-1 my-2">
+    
+                    <div class="flex flex-row gap-3">
+                        <!-- profile picture -->
+                        <div class="relative ">
+                            <img class="w-[40px] h-[40px] rounded-full" src="${x.authors[0].profile_picture}" alt="">
+                           <div class="absolute  -mt-[88px] left-[184px]">
+                            <p class="text-white bg-black text-center p-1 w-[110px] text-xs">3hrs 56 min ago</p>
+                           </div>
+                        </div>
+    
+                        <!-- card description -->
+                        <div class="flex-1">
+                            <h2 class="text-lg font-bold">${x.title}</h2>
+                            <div class="my-2">
+                                <p class="inline-block">${x.authors[0].profile_name}</p>
+                                <span>${x.authors[0].verified ? x.authors[0].verified : ''}<i class="fa-solid text-blue-500 fa-circle-check ml-2"></i></span>
+                            </div>
+                            <p>${x.others.views} views</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+            cardContainer.appendChild(cardDiv);
+        })
+    }
+
+    else{
+        const noContentDiv = document.createElement('div');
+        noContentDiv.innerHTML = `
+        <div class=" max-w-2xl  text-center flex flex-col justify-center items-center mx-auto">
+                    <img class="mt-32 md:mt-48 w-[140px]" src="./images/icon.png" alt="">
+                    <h1 class="text-3xl mt-8 font-bold">Oops!! Sorry, There is no <br> content here</h1>
+                </div>
+        `;
+        noContentContainer.appendChild(noContentDiv);
+    }
+
+
+    console.log(data.data);
+
 }
 handleCategory();
