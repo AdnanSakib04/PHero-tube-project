5d01404a4397828d4a477ad32a31ca54e7b2388d
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-01 14:00:38 +0600

    sorting done

--- phtube.js
+++ phtube.js
@@ -1,3 +1,4 @@
+let currentCategoryID;
 const handleCategory = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
@@ -11,13 +12,15 @@ const handleCategory = async () => {
         <a onclick="handleLoadCategory('${category.category_id}')" class="btn w-[104px] tab bg-[#25252526]">${category.category}</a>
         `;
         tabsContainer.appendChild(div);
+
     })
 
 }
 
 
-
 const handleLoadCategory = async (id) => {
+    currentCategoryID = id;
+
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await response.json();
 
@@ -27,24 +30,24 @@ const handleLoadCategory = async (id) => {
     const noContentContainer = document.getElementById('no-content-container');
     noContentContainer.innerHTML = "";
 
-    console.log('--------------length');
-    console.log(data.data.length);
+    // console.log('--------------length----------');
+    // console.log(data.data.length);
 
     const checkLength = data.data.length;
     if (checkLength > 0) {
         data.data.forEach((x) => {
-            console.log(x.thumbnail);
-            console.log(x.title);
-            console.log(x.authors[0].profile_picture);
-            console.log(x.authors[0].profile_name);
-            console.log(x.authors[0].verified);
-            console.log(x.others.views);
-            console.log(x.others.posted_date);
+            // console.log(x.thumbnail);
+            // console.log(x.title);
+            // console.log(x.authors[0].profile_picture);
+            // console.log(x.authors[0].profile_name);
+            // console.log(x.authors[0].verified);
+            // console.log(x.others.views);
+            // console.log(x.others.posted_date);
 
             // calculating the hours and minute
             let totalSeconds = x.others.posted_date;
             const hours = parseInt(totalSeconds / 3600);
-            totalSeconds = totalSeconds% 3600;
+            totalSeconds = totalSeconds % 3600;
             const mins = parseInt(totalSeconds / 60);
 
             const cardDiv = document.createElement('div');
@@ -58,7 +61,7 @@ const handleLoadCategory = async (id) => {
                         <!-- profile picture -->
                         <div class="relative ">
                             <img class="w-[40px] h-[40px] rounded-full" src="${x.authors[0].profile_picture}" alt="">
-                            ${x.others.posted_date? `<div class="absolute  -mt-[88px] left-[166px]">
+                            ${x.others.posted_date ? `<div class="absolute  -mt-[88px] left-[166px]">
                             <p class="text-white bg-[#171717] text-center p-1 w-[130px] text-xs">${hours}hrs ${mins} min ago</p>
                            </div>` : ''}
                            
@@ -81,7 +84,7 @@ const handleLoadCategory = async (id) => {
         })
     }
 
-    else{
+    else {
         const noContentDiv = document.createElement('div');
         noContentDiv.innerHTML = `
         <div class=" max-w-2xl  text-center flex flex-col justify-center items-center mx-auto">
@@ -93,7 +96,99 @@ const handleLoadCategory = async (id) => {
     }
 
 
-    console.log(data.data);
+    // console.log(data.data);
+
+}
+
+
+const handleSortByView = async () => {
+    console.log(currentCategoryID);
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${currentCategoryID}`);
+    const data = await response.json();
+
+    console.log(parseFloat(data.data[0].others.views.split('K')[0]));
+
+    const cardContainer = document.getElementById('cards-container');
+    cardContainer.innerHTML = "";
+
+    const noContentContainer = document.getElementById('no-content-container');
+    noContentContainer.innerHTML = "";
+
+    const checkLength = data.data.length;
+    if (checkLength > 0) {
+
+        // console.log('before sorting');
+        // for (let i = 0; i < checkLength; i++) {
+        //     console.log(parseFloat(data.data[i].others.views.split('K')[0]));
+        // }
+
+        for (let i = 0; i < checkLength; i++) {
+            for (let j = i+1; j < checkLength; j++) {
+                if (parseFloat(data.data[i].others.views.split('K')[0]) < parseFloat(data.data[j].others.views.split('K')[0])) {
+                    const temp = data.data[i];
+                    data.data[i] = data.data[j];
+                    data.data[j] = temp;
+
+                }
+            }
+        }
+
+        // console.log('after sorting');
+        // for (let i = 0; i < checkLength; i++) {
+        //     console.log(parseFloat(data.data[i].others.views.split('K')[0]));
+        // }
+
+        data.data.forEach((x) => {
+            // calculating the hours and minute
+            let totalSeconds = x.others.posted_date;
+            const hours = parseInt(totalSeconds / 3600);
+            totalSeconds = totalSeconds % 3600;
+            const mins = parseInt(totalSeconds / 60);
+
+            const cardDiv = document.createElement('div');
+            cardDiv.innerHTML = `
+            <!-- card 1 -->
+            <div class="card card-compact w-[312px] h-[300px] bg-base-100 shadow-xl relative">
+                <figure><img  src="${x.thumbnail}" alt="" /></figure>
+                <div class="p-1 my-2">
+    
+                    <div class="flex flex-row gap-3">
+                        <!-- profile picture -->
+                        <div class="relative ">
+                            <img class="w-[40px] h-[40px] rounded-full" src="${x.authors[0].profile_picture}" alt="">
+                            ${x.others.posted_date ? `<div class="absolute  -mt-[88px] left-[166px]">
+                            <p class="text-white bg-[#171717] text-center p-1 w-[130px] text-xs">${hours}hrs ${mins} min ago</p>
+                           </div>` : ''}
+                           
+                        </div>
+    
+                        <!-- card description -->
+                        <div class="flex-1">
+                            <h2 class="text-lg font-bold">${x.title}</h2>
+                            <div class="my-2">
+                                <p class="inline-block">${x.authors[0].profile_name}</p>
+                                <span>${x.authors[0].verified ? `<i class="fa-solid text-blue-500 fa-circle-check ml-2"></i>` : ''}</span>
+                            </div>
+                            <p>${x.others.views} views</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+            cardContainer.appendChild(cardDiv);
+        })
+    }
+
+    else {
+        const noContentDiv = document.createElement('div');
+        noContentDiv.innerHTML = `
+        <div class=" max-w-2xl  text-center flex flex-col justify-center items-center mx-auto">
+                    <img class="mt-32 md:mt-48 w-[140px]" src="./images/icon.png" alt="">
+                    <h1 class="text-3xl mt-8 font-bold">Oops!! Sorry, There is no <br> content here</h1>
+                </div>
+        `;
+        noContentContainer.appendChild(noContentDiv);
+    }
 
 }
 handleCategory();
